f9b9b3cd79ffe6001c8f65d38dd8e6c9250213c9
Mahdi Hasan <mhasan@westminster.gov.uk>
2023-01-31 00:35:38 +0000

    A lot to do with CSS, dynamic styles and different styling methods

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
-import './CourseGoalList.css';
+import CourseGoalItem from "../CourseGoalItem/CourseGoalItem";
+import "./CourseGoalList.css";
 
-const CourseGoalList = props => {
+const CourseGoalList = (props) => {
   return (
     <ul className="goal-list">
-      {props.items.map(goal => (
+      {props.items.map((goal) => (
         <CourseGoalItem
           key={goal.id}
           id={goal.id}
           onDelete={props.onDeleteItem}
         >
+          {/* The things between this element is passed as props.children for
+          "CourseGoalItem" */}
           {goal.text}
         </CourseGoalItem>
       ))}
